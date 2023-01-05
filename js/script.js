59b9d9efebcfdc9116dca500c2e4272cf6927fbc
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-01-05 09:53:14 +0100

    added

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// Question 1
+
+const wholeNumber = 300;
+const floatNumber = 80.2;
+
+// Question 2
+
+const weekDays = "monday - tuesday - wednesday - thursday - friday";
+
+// Question 3
+
+const addidtion = 1 + 1;
+const subtraction = 2 - 1;
+const multiplication = 3 * 3;
+const division = 9 % 3;
+
+// Question 4
+
+const isTrue= true;
+const isFalse= false;
+
+// Question 5
+
+const numberOfLetters = "The frogs are angry."
+console.log(numberOfLetters.length);
+
+
+// Question 6
+
+let timeElapsed = "time_elapsed";
+let totalPrice = "totalprice";
+
+// Question 7
+
+var number1 = "53";
+var number2 = "44.6";
+
+console.log(parseInt(number1));
+
+console.log(parseFloat(number2));
+
+// Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+
+console.log(typeof pet);
+console.log(typeof age);
+console.log(typeof discountApplied);
+console.log(typeof timeLeft);
+
+// Question 9
+
+const newHeadingContent = "Updating heading";
+const headingElement = document.querySelector("h1");
+headingElement.innerHTML = newHeadingContent;
+
+
+document.body.style.backgroundColor = "lightskyblue";
+document.addEventListener("click", function() {
+    document.h1.style.backgroundColor = "green";
+  });
+
+
+
+
+
+
+
+
